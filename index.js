5554bb93fada5661d4585e66697f6f07e278abc1
prince4utd <chukwuprince68@gmail.com>
2026-06-27 18:26:26 +0100

    business-card

---
+++ index.js
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <main>
+    <img/>
+    <h1>Fun fact about react</h1>
+    <ul>
+      <li>Was firt release in 2013</li>
+      <li>Was originally created byt jordan walke</li>
+      <li>Was well over 200k starts on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thausands of enterprise app, including mobile apps</li>
+    </ul>
+  </main>
+)
